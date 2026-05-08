1264ffe7f61b7a0a46a96198b36feae63e34f5ea
Gangareddy Nachu <gnachu@horizon.csueastbay.edu>
2026-05-08 11:03:14 -0700

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        let resLen = 0;
+        let resIdx = 0;
+        let count = 0;
+        for(let i=0;i<s.length;i++){
+            //odd length
+            let l = i;
+            let r = i;
+            while(l>=0 && r<s.length && s.charAt(l)===s.charAt(r)){
+                count++
+                l--;
+                r++;
+            }
+            //even length
+            l = i;
+            r = i+1;
+            while(l>=0 && r<s.length && s.charAt(l) === s.charAt(r)){
+                count++;
+                l--;
+                r++;
+            }
+        }
+        return count;
+    }
+}
