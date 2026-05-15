b6eb1165f023ab325d8a83e1740df0f7185eee85
Gangareddy Nachu <gnachu@horizon.csueastbay.edu>
2026-05-15 12:16:26 -0700

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        /* Decision is to buy or sell
+        // [1,3,4,0,4]
+        // buy-> sell ->
+
+        */
+        const dp = {};
+        const dfs = (i, buying) => {
+            if (i >= prices.length) {
+                return 0;
+            }
+            let key = `${i}-${buying}`;
+            if (key in dp) {
+                return dp[key];
+            }
+
+            let cooldown = dfs(i + 1, buying);
+            if (buying) {
+                let buy = dfs(i + 1, false) - prices[i];
+                dp[key] = Math.max(buy, cooldown);
+            } else {
+                let sell = dfs(i + 2, true) + prices[i];
+                dp[key] = Math.max(sell, cooldown);
+            }
+            return dp[key];
+        };
+
+        return dfs(0, true);
+    }
+}
