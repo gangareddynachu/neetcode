931ca8f5c4de40f0af22ff9b28b94200d4e645a7
Gangareddy Nachu <gnachu@horizon.csueastbay.edu>
2026-09-04 22:20:10 -0700

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} digits
+     * @return {number[]}
+     */
+    plusOne(digits) {
+        for (let i = digits.length - 1; i >= 0; i--) {
+            //If none of digits starting from last are 9, then we simply add 1 over there and return.
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        // All digits were 9 (e.g. [9,9,9] -> [1,0,0,0])
+        digits.unshift(1);
+        return digits;
+    }
+}
